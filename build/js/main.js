2ae8e1b0fa2a3d78920176a63760cf44f03a9715
Oleh-Moroz <oleh.moroz@utechcorp.com>
2021-12-20 17:48:15 +0200

    fix link

--- build/js/main.js
+++ build/js/main.js
@@ -13798,7 +13798,7 @@ function openedSearchDropdown() {
     let inputValue = searchInput.value;
 
     searchForm.classList.add('search-active');
-    searchResults.innerHTML = `<li><a href="/view/product/search.html?s=${inputValue}">${inputValue}</a></li>`;
+    searchResults.innerHTML = `<li><a href="../../view/product/search.html?s=${inputValue}">${inputValue}</a></li>`;
     if (searchDropdown.classList.contains('open-no-results')) {
         searchDropdown.classList.remove('open-no-results');
         searchDropdown.classList.add('open');
@@ -13825,7 +13825,7 @@ searchForm.addEventListener('submit', (e) => {
 
     let inputValue = searchInput.value;
     if (searchInput.value) {
-        window.location.href = `/view/product/search.html?s=${inputValue}`;
+        window.location.href = `../../view/product/search.html?s=${inputValue}`;
     } else {
         searchForm.classList.add('search-active');
         searchDropdown.classList.add('open-no-results');
